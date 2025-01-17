bd40dc123c2d77bd4a3df6c53e6579a1d498cbae
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2025-01-17 17:30:05 -0500

    Add playGame function to simulate a five round game of Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -22,20 +22,47 @@ function getHumanChoice(){
     }
 }
 
-
-let humanScore = 0; 
-let computerScore = 0; 
-
-function playRound(humanChoice, computerChoice){
-    if (humanChoice == computerChoice){
-        console.log("It's a tie!");
-    } else if (humanChoice == "ROCK" && computerChoice == "SCISSORS" || 
-        humanChoice =="PAPER" && computerChoice == "ROCK" ||
-        humanChoice == "SCISSORS" && computerChoice == "PAPER"){
-        humanScore++; 
-        console.log(`You Win! ${humanChoice} beats %{computerChoice}!`);
-    } else {
-        console.log(`You lose! ${computerChoice} beats ${playerChoice}!`);
-        computerScore++; 
+function playGame(){
+    
+    function playRound(humanChoice, computerChoice){
+        if (humanChoice == computerChoice){
+            console.log("It's a tie!");
+        } else if (humanChoice == "ROCK" && computerChoice == "SCISSORS" || 
+            humanChoice =="PAPER" && computerChoice == "ROCK" ||
+            humanChoice == "SCISSORS" && computerChoice == "PAPER"){
+            humanScore++; 
+            console.log(`You Win! ${humanChoice} beats %{computerChoice}!`);
+        } else {
+            console.log(`You lose! ${computerChoice} beats ${playerChoice}!`);
+            computerScore++; 
+        }
     }
+
+    for (let i = 0; i < 5; i++){
+        console.log(`Round ${i + 1}`); 
+        let humanScore = 0; 
+        let computerScore = 0; 
+
+        const winner = playRound(humanChoice, computerChoice);
+
+        // Update scores
+        if (winner === "human") {
+            humanScore++;
+        } else if (winner === "computer") {
+            computerScore++;
+        }
+
+        // Display current scores
+        console.log(`Current Score: Human - ${humanScore}, Computer - ${computerScore}`);
+    }
+
+    // Declare the final winner
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You won the game!");
+    } else if (computerScore > humanScore) {
+        console.log("You lost the game. Better luck next time!");
+    } else {
+        console.log("It's a tie overall!");
+    }
+
 }
